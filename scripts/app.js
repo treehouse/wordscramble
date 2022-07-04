874556b9850504d6b1fb579bddc86b3a61cab9d6
dustinusey <dustinusey@me.com>
2022-07-04 15:27:31 -0500

    static UI

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,19 @@
 let root = document.documentElement;
+
+let time = 10;
+let timer = document.querySelector('.timer');
+const pulse = document.querySelector('.pulse');
+timer.textContent = time;
+
+const timerInterval = setInterval(() => {
+    timer.textContent = time;
+    time -=1;
+    if (time == 0) {
+        clearInterval(timerInterval);
+        timer.textContent = 'X';
+        pulse.style.animation = 'none';
+        pulse.classList.add('gameover');
+    }
+}, 1000);
+
+
