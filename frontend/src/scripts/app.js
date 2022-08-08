a5c902dcf37a9a9cfd08037a4b9b36fc18236034
dusey <dustinusey@me.com>
2022-08-08 11:07:09 -0500

    modal UI and countdown timer

--- frontend/src/scripts/app.js
+++ frontend/src/scripts/app.js
@@ -1,6 +1,29 @@
-const startGame = document.getElementById('startNewGame');
+const startGame = document.getElementById('startNewGame')
+const gameBoard = document.getElementById('gameboard');
 const overlay = document.getElementById('overlay');
+const howToPlayBtn = document.getElementById('howToPlay');
+const modalContainer = document.getElementById('modalContainer');
+const closeModalBtn = document.getElementById('closeModal');
 
 startGame.addEventListener('click', () => {
-    overlay.style.display = 'none';
+    howToPlayBtn.setAttribute('disabled', true);
+    let countdown = 5;
+
+    startGame.textContent = `Starting in ${countdown}...`
+    setInterval(() => {
+        countdown --;
+        startGame.textContent = `Starting in ${countdown}...`;
+    }, 1000)
+    setTimeout(() => {
+        overlay.style.display = 'none';
+        gameBoard.style.display = 'flex';
+    }, 5000)
 });
+
+howToPlayBtn.addEventListener('click', () => {
+    modalContainer.classList.add('active');
+});
+
+closeModalBtn.addEventListener('click', () => {
+    modalContainer.classList.remove('active');
+})
